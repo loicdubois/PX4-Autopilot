442bfb107e61038000e98183807db190200783db
Loïc Dubois <loc.21.dubois@gmail.com>
2025-03-23 11:20:22 +0100

    reorganize help scripts

--- flash_typhoon_stlink.sh
+++ flash_typhoon_stlink.sh
@@ -1,1 +1,2 @@
+make yuneec_typhoon_h
 st-flash write build/yuneec_typhoon_h_default/yuneec_typhoon_h.bin 0x8008000
--- px4_typhoon.sh
+++
@@ -1,1 +0,0 @@
-make clean; make yuneec_typhoon_h; st-flash write build/yuneec_typhoon_h_default/yuneec_typhoon_h.bin 0x8008000
